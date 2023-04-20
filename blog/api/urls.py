8f79b21a424ef95716681e991a7e400924c7bbc2
putongyong <117098471+putongyong@users.noreply.github.com>
2023-04-20 16:11:11 +0200

    updated views

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from api import views
 
 urlpatterns = [
     path('blogs/', views.blog_list),
     path('blogs/<int:pk>/', views.blog_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- blog/api/views.py
+++ blog/api/views.py
@@ -1,52 +1,64 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
+#from django.http import HttpResponse, JsonResponse
+#from django.views.decorators.csrf import csrf_exempt
+#from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from api.models import Blog
 from api.serializers import BlogSerializer
 
-@csrf_exempt
-def blog_list(request):
+@api_view(['GET', 'POST'])
+def blog_list(request, format=None):
     """
     List all code snippets, or create a new snippet.
     """
     if request.method == 'GET':
         blogs = Blog.objects.all()
         serializer = BlogSerializer(blogs, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
+        #return JsonResponse(serializer.data, safe=False)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
+        #data = JSONParser().parse(request)
         serializer = BlogSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            #return JsonResponse(serializer.data, status=201)
+        #return JsonResponse(serializer.errors, status=400)
     
-@csrf_exempt
-def blog_detail(request, pk):
+@api_view(['GET', 'PUT', 'DELETE'])
+def blog_detail(request, pk, format=None):
     """
     Retrieve, update or delete a code snippet.
     """
     try:
         blog = Blog.objects.get(pk=pk)
     except Blog.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
+        #return HttpResponse(status=404)
 
     if request.method == 'GET':
         serializer = BlogSerializer(blog)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
+        #return JsonResponse(serializer.data)
 
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = BlogSerializer(blog, data=data)
+        #data = JSONParser().parse(request)
+        #serializer = BlogSerializer(blog, data=data)
+        serializer = BlogSerializer(blog, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            #return JsonResponse(serializer.data)
+        #return JsonResponse(serializer.errors, status=400)
 
     elif request.method == 'DELETE':
         blog.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+        #return HttpResponse(status=204)
