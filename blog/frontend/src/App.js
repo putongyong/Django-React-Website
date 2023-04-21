37273398fd6a9c944cd53af9e412f2cf792cf9e2
putongyong <117098471+putongyong@users.noreply.github.com>
2023-04-21 11:43:06 +0200

    solved the favicon problem

--- blog/frontend/src/App.js
+++ blog/frontend/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import icon from './images/favicon.ico';
+import React, { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    const favicon = document.getElementById('favicon');
+    favicon.setAttribute('href', icon);
+}, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello World!
     </div>
   );
 }
