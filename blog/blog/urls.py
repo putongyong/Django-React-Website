fa5a1644f77cc952ee3b450a24bf757f0798a643
putongyong <117098471+putongyong@users.noreply.github.com>
2023-04-25 17:11:52 +0200

    try to hide api urls

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from api.views import front
+#from rest_framework import routers
 
+#router = routers.SimpleRouter()
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    #path('api/', include((router.urls, 'api'))),
     path('', front, name="front"),
 ]
