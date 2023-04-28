89fec205606d651e381fc29b67f4b947b6a19e57
putongyong <117098471+putongyong@users.noreply.github.com>
2023-04-28 10:37:09 +0200

    repath problem solved

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework.routers import SimpleRouter
 from api.views import front, BlogViewSet
 
@@ -23,6 +23,8 @@ router.register('blogs', BlogViewSet, basename='blog')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', front, name='front'),
     path('api/', include(router.urls)),
+    path('', front, name='front'),
+    re_path(".*", front, name='front'), 
+    #path("__reload__", include("django_browser_reload.urls")),
 ]
